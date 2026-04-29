26f0da4caa5853c42620659fdf14c3d17d8f7fb8
Sunny <sunny@Sunnys-MacBook-Air.local>
2026-04-29 17:44:48 +0530

    build: remove vite legacy plugin to optimize build times

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'safari >= 13'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
+    react()
   ],
 })
